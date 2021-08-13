c204291fe4995dac7aa520014a47e41d6cd48c95
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-08-13 17:55:40 +0900

    Feat skip whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,6 +27,8 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -67,6 +69,12 @@ func (l *Lexer) readIdentifier() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func isLetter(ch byte) bool {
