780b6f03c4747ac507c38362a3fe206b2d638ccd
HMasataka <siruba.2050@gmail.com>
2022-04-14 22:02:40 +0900

    Feat integer object

---
+++ object/object.go
@@ -0,0 +1,16 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
